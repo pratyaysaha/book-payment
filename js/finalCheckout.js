7bfd2d213891c0d575ab6bb1fa934c2204d0b7b1
pratyaysaha <pratyaysaha@outlook.com>
2021-08-24 20:27:05 +0530

    production-mode

--- js/finalCheckout.js
+++ js/finalCheckout.js
@@ -5,13 +5,14 @@ if (!document.querySelector(".purcahse.email")) {
 	email = document.querySelector(".purcahse.email").innerHTML.trim()
 }
 var options = {
-	key: "rzp_test_VRXPbBudsU0xFQ", // Enter the Key ID generated from the Dashboard
-	//key: "rzp_live_UtV07y4Iq3FFX9",
+	//key: "rzp_test_VRXPbBudsU0xFQ", // Enter the Key ID generated from the Dashboard
+	key: "rzp_live_UtV07y4Iq3FFX9",
 	amount: "170", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
 	currency: "INR",
 	name: "Himadri Saha",
 	description: "Payment for Aakul Brikhsho",
-	image: "https://www.logodesign.net/logo/crane-truck-with-hook-4110ld.png",
+	image:
+		"https://res.cloudinary.com/psaha/image/upload/f_auto/v1629816759/book-cover_myr4zl.jpg",
 	order_id: document.querySelector(".input.orderId").value, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
 	callback_url: "/api/order/afterpayment",
 	prefill: {
